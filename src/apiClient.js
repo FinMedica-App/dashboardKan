290cc6b299df001b48538ab9a573e899f54d491b
Steven Ramírez Salgado <sramirez@finsalud.onmicrosoft.com>
2025-02-05 13:30:02 -0600

    subiendo el servicio correcto

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000/proxy";
+const baseURL = import.meta.env.VITE_API_URL || "http://54.80.127.136:4000/proxy";
 
 const apiClient = axios.create({
   baseURL, 
